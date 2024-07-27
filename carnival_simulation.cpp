4015857f2c3808511afa52a8b12b0bf3d231f9de
AdityaKannur <adityakannur28@gmail.com>
2024-07-27 18:09:53 +0530

    used this pointer

--- carnival_simulation.cpp
+++ carnival_simulation.cpp
@@ -1,67 +1,81 @@
 #include <iostream>
 using namespace std;
 
-
 class Rides{
     private:
-    std::string ride_name;
+    string ride_name;
     double ride_duration;
-    int fare;
+    int ride_fare;
 
     public:
-    void setName(std::string name){
-        ride_name = name;
-    }
-    void setDuration(double duration){
-        ride_duration = duration;
-    }
-    std::string getName(){
-        return ride_name;
+
+    Rides(string name, double duration, int fare){
+        this->ride_name = name;
+        this-> ride_duration = duration;
+        this-> ride_fare = fare;
+    };
+
+    // void setName(string name){
+    //     this->ride_name = name;
+    // }
+    // void setDuration(double duration){
+    //     this->ride_duration = duration;
+    // }
+    string getName(){
+        return this->ride_name;
     }
     double getDuration(){
-        return ride_duration;
+        return this->ride_duration;
     }
 };
 
 class Stalls{
     private:
-    std::string stall_name;
+    string stall_name;
     double stall_timer;
-    int fare;
+    int stall_fare;
 
     public:
-    void setName(std::string name){
-        stall_name = name;
-    }
-    void setTimer(double timer){
-        stall_timer = timer;
-    }
-    std::string getName(){
-        return stall_name;
+
+    Stalls(string stall_name, double stall_timer, int stall_fare){
+        this-> stall_name = stall_name;
+        this-> stall_timer = stall_timer;
+        this-> stall_fare = stall_fare;
+    };
+
+
+    // void setName(string name){
+    //     this->stall_name = name;
+    // }
+    // void setTimer(double timer){
+    //     this->stall_timer = timer;
+    // }
+    string getName(){
+        return this->stall_name;
     }
     double getTimer(){
-        return stall_timer;
+        return this->stall_timer;
     }
 };
 
 int main(){
-    Rides roller_coster;
-    Stalls game_stall;
+    Rides roller_coster("roller-coster", 3, 300);
+    Stalls game_stall("shooting", 2, 200);
 
-    std::string ride_name = "roller_coster";
-    double ride_duration = 2; // in minutes
-    std::string stall_name = "shooting_game";
-    double stall_timer = 3; // in minutes
+    // string ride_name = "roller_coster";
+    // double ride_duration = 2; // in minutes
+    // string stall_name = "shooting_game";
+    // double stall_timer = 3; // in minutes
 
-    roller_coster.setName(ride_name);
-    roller_coster.setDuration(ride_duration);
-    game_stall.setName(stall_name);
-    game_stall.setTimer(stall_timer);
+    // roller_coster.setName(ride_name);
+    // roller_coster.setDuration(ride_duration);
+    // game_stall.setName(stall_name);
+    // game_stall.setTimer(stall_timer);
 
-    std::cout << "Ride name:" << " " << roller_coster.getName() << " " << endl;
-    std::cout << "Ride duration: " << " " << roller_coster.getDuration() << " " << endl;
-    std::cout << "stall name: " << " " << game_stall.getName() << " " << endl;
-    std::cout << "stall duration: " << " " << game_stall.getTimer();
+    cout << "Ride name:" << " " << roller_coster.getName() << " " << endl;
+    cout << "Ride duration: " << " " << roller_coster.getDuration() << " " << endl;
+    cout << "stall name: " << " " << game_stall.getName() << " " << endl;
+    cout << "stall duration: " << " " << game_stall.getTimer();
 
    return 0; 
 }
